ae08ff4fea991100d7c25eeaae687cc42c0a20aa
Siddhant Regmi <siddhantregmi121@gmail.com>
2025-02-09 13:50:11 +0545

    completed Transactions CRUD

--- src/transactions/transactions.service.ts
+++ src/transactions/transactions.service.ts
@@ -1,26 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { UpdateTransactionDto } from './dto/update-transaction.dto';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class TransactionsService {
-  create(createTransactionDto: CreateTransactionDto) {
-    return 'This action adds a new transaction';
+  constructor(private readonly prisma: PrismaClient) {}
+  async create(createTransactionDto: CreateTransactionDto) {
+    return this.prisma.transaction.create({
+      data: createTransactionDto,
+    });
   }
 
-  findAll() {
-    return `This action returns all transactions`;
+  async findAll() {
+    return this.prisma.transaction.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} transaction`;
+  async findOne(id: number) {
+    return this.getTransaction(id);
   }
 
-  update(id: number, updateTransactionDto: UpdateTransactionDto) {
-    return `This action updates a #${id} transaction`;
+  async update(id: number, updateTransactionDto: UpdateTransactionDto) {
+    const transaction = await this.getTransaction(id);
+    return this.prisma.transaction.update({
+      where: { id },
+      data: updateTransactionDto,
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} transaction`;
+  async remove(id: number) {
+    await this.getTransaction(id);
+    return this.prisma.transaction.delete({ where: { id } });
+  }
+
+  private async getTransaction(id: number) {
+    const transaction = await this.prisma.transaction.findFirst({
+      where: { id },
+    });
+    if (!transaction) {
+      throw new NotFoundException('Transaction not found');
+    }
+    return transaction;
   }
 }
